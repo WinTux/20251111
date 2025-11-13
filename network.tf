f4d82414d8bb171650af57584363964dfded5d8c
Rusok <rusok.software@gmail.com>
2025-11-13 12:09:21 -0400

    agregado

--- network.tf
+++ network.tf
@@ -19,7 +19,7 @@ module "security-group" {
   name = "Reglas para spring"
   vpc_id = module.vpc.vpc_id
   description = "Security Group para proyecto Terraform"
-  ingress_rules = ["https-443-tcp", "ssh-tcp", "http-80-tcp"]
+  ingress_rules = ["https-443-tcp", "ssh-tcp", "http-8080-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
